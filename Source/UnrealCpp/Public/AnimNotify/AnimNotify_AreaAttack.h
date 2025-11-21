26055afb66b09e30d4b788c0dbee2fd3507d37b6
kdjh2002 <kdjh2002@naver.com>
2025-11-21 17:53:27 +0900

    범위 공격하기 클래스생성

---
+++ Source/UnrealCpp/Public/AnimNotify/AnimNotify_AreaAttack.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AnimNotify_AreaAttack.generated.h"
+
+//애님노티파이어임으로 신호한번만 보내면됨
+/**
+ * 
+ */
+UCLASS()
+class UNREALCPP_API UAnimNotify_AreaAttack : public UAnimNotify
+{
+	GENERATED_BODY()
+
+	//범위공격은 캐릭터가 결정
+
+public:
+	virtual void 
+
+private:
+	TWeakObjectPtr<class AActionCharacter> OwnerCharacter = nullptr;
+	
+};
